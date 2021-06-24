b18e88e5c3452a23f24cfb862b549627b5892c9b
Casteyes <casteyestothesun@gmail.com>
2021-06-24 14:10:28 -0400

    Full Chat Screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import ChatListItem from '../components/ChatListItem';
 import EditScreenInfo from '../components/EditScreenInfo';
 import {View } from '../components/Themed';
 import chatRooms from '../data/ChatRooms'
 
 
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <ChatListItem chatRoom={chatRooms[0]}/>
+     <FlatList 
+     data={chatRooms} 
+     renderItem={({item}) => <ChatListItem chatRoom={item} />} />
     </View>
   );
 }
