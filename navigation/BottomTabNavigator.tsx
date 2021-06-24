ce93404339e09b425a8cb2352abf6c2644ce6b03
Casteyes <casteyestothesun@gmail.com>
2021-06-23 23:42:23 -0400

    first commit

---
+++ navigation/BottomTabNavigator.tsx
@@ -0,0 +1,103 @@
+/**
+ * Learn more about createBottomTabNavigator:
+ * https://reactnavigation.org/docs/bottom-tab-navigator
+ */
+
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+
+import{createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import TabOneScreen from '../screens/TabOneScreen';
+import TabTwoScreen from '../screens/TabTwoScreen';
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+
+const BottomTab = createMaterialTopTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Chats"
+      tabBarOptions={{
+          activeTintColor: Colors[colorScheme].background,
+          style:{
+            backgroundColor: Colors[colorScheme].tint,  
+          },
+          indicatorStyle:{ 
+          backgroundColor: Colors[colorScheme].background,
+          height:3,
+          },
+          labelStyle: {
+            fontWeight: 'bold',
+          }
+            
+          }}>
+      <BottomTab.Screen
+        name="Chats"
+        component={TabOneNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Friends"
+        component={TabTwoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+        <BottomTab.Screen
+        name="Teams"
+        component={TabTwoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const TabOneStack = createStackNavigator<TabOneParamList>();
+
+function TabOneNavigator() {
+  return (
+    <TabOneStack.Navigator>
+      <TabOneStack.Screen
+        name="TabOneScreen"
+        component={TabOneScreen}
+        options={{ 
+          headerShown: false,
+          headerTitle: 'Tab One Title' }}
+      />
+    </TabOneStack.Navigator>
+  );
+}
+
+const TabTwoStack = createStackNavigator<TabTwoParamList>();
+
+function TabTwoNavigator() {
+  return (
+    <TabTwoStack.Navigator>
+      <TabTwoStack.Screen
+        name="TabTwoScreen"
+        component={TabTwoScreen}
+        options={{ 
+          headerShown: false,
+          headerTitle: 'Tab Two Title' }}
+      />
+    </TabTwoStack.Navigator>
+  );
+}
---
+++ types.tsx
@@ -0,0 +1,42 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Chats: undefined;
+  Friends: undefined;
+  Teams: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type User = {
+  id: String;
+  name: String;
+  imageUri: String;
+}
+
+export type Message = {
+  id: String;
+  content: string;
+  createdAt: string;
+
+}
+
+export type ChatRoom ={
+id: String;
+users: [User];
+lastMessage: Message;
+};
